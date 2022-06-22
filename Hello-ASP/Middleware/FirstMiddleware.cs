a01a554fafc33d2573edb637c4116746602235ae
Luong Hoang <hdluong1209@gmail.com>
2022-06-22 08:13:25 +0700

    ASP-Middleware

---
+++ Hello-ASP/Middleware/FirstMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_ASP
+{
+    public class FirstMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FirstMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"URL: {context.Request.Path}");
+            context.Items.Add("DataFirstMiddleware", $"<p>URL: {context.Request.Path}</p>");
+            await _next(context);
+        }
+    }
+}
---
+++ Hello-ASP/Middleware/SecondMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_ASP
+{
+    public class SecondMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (context.Request.Path == "/xxx.html")
+            {
+                context.Response.Headers.Add("SecondMiddleware", "Ban khong duoc truy cap");
+                
+                var datafromFirstMiddleware = context.Items["DataFirstMiddleware"];
+                if (datafromFirstMiddleware != null)
+                {
+                    await context.Response.WriteAsync((string)datafromFirstMiddleware);
+                }
+
+                await context.Response.WriteAsync("Ban khong duoc truy cap");
+            }
+            else
+            {
+                context.Response.Headers.Add("SecondMiddleware", "Ban duoc truy cap");
+                var datafromFirstMiddleware = context.Items["DataFirstMiddleware"];
+                if (datafromFirstMiddleware != null)
+                {
+                    await context.Response.WriteAsync((string)datafromFirstMiddleware);
+                }
+
+                await next(context);
+            }
+        }
+    }
+}
