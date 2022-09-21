445069c5dfe5f7698025fead38912c171b9c5894
Luong Hoang <hdluong1209@gmail.com>
2022-09-21 17:02:58 +0700

    160. Intersection of Two Linked Lists

--- LeetCode/LinkedListSolution.cs
+++ LeetCode/LinkedListSolution.cs
@@ -97,5 +97,87 @@ namespace LeetCode
 
             return null;
         }
+
+        /// <summary>
+        /// 160. Intersection of Two Linked Lists
+        /// </summary>
+        /// <param name="headA"></param>
+        /// <param name="headB"></param>
+        /// <returns></returns>
+        public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            var n1 = headA;
+            var n2 = headB;
+
+            while (n1 != n2)
+            {
+                n1 = n1 == null ? headB : n1.next;
+                n2 = n2 == null ? headA : n2.next;
+            }
+
+            return n1;
+        }
+
+        /// <summary>
+        /// [4, 1, 8, 4, 5]
+        /// [5, 6, 1, 8, 4, 5]
+        /// </summary>
+        /// <param name="headA"></param>
+        /// <param name="headB"></param>
+        /// <returns></returns>
+        public static ListNode GetIntersectionNode1(ListNode headA, ListNode headB)
+        {
+            var p1 = headA;
+            var p2 = headB;
+            var length1 = 0;
+            var length2 = 0;
+
+            while (p1 != null || p2 != null)
+            {
+                if (p1 != null)
+                {
+                    p1 = p1.next;
+                    length1++;
+                }
+
+                if (p2 != null)
+                {
+                    p2 = p2.next;
+                    length2++;
+                }
+            }
+
+            var k = Math.Abs(length1 - length2);
+            p1 = headA;
+            p2 = headB;
+
+            while (p1 != null && p2 != null)
+            {
+                if (k != 0)
+                {
+                    if (length1 > length2)
+                    {
+                        p1 = p1.next;
+                    }
+                    else if (length1 < length2)
+                    {
+                        p2 = p2.next;
+                    }
+
+                    k--;
+                    continue;
+                }
+
+                if (p1 == p2)
+                {
+                    return p1;
+                }
+
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+
+            return null;
+        }
     }
 }
