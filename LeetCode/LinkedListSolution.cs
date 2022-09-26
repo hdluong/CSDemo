ed97d3ea7fe9228095c9d5f4f7f3d05cb1fa5dea
Luong Hoang <hdluong1209@gmail.com>
2022-09-26 16:59:08 +0700

    leetcode - 328. Odd Even Linked List

--- LeetCode/LinkedListSolution.cs
+++ LeetCode/LinkedListSolution.cs
@@ -316,27 +316,75 @@ namespace LeetCode
             return head;
         }
 
+        /// <summary>
+        /// 328. Odd Even Linked List
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static ListNode OddEvenList(ListNode head)
+        {
+            if (head == null || head.next == null || head.next.next == null)
+            {
+                return head;
+            }
+
+            ListNode first = head;
+            ListNode second = head.next;
+            ListNode next = null;
+
+            var odd = first;
+            var even = second;
+            head = even.next;
+
+            while (head != null)
+            {
+                if (head.next != null)
+                {
+                    next = head.next.next;
+                }
+                else
+                {
+                    next = head.next;
+                }
+
+                odd.next = head;
+                even.next = head.next;
+                odd = odd.next;
+                even = even.next;
+
+                head = next;
+            }
+
+            if (even != null)
+            {
+                even.next = null;
+            }
+            odd.next = second;
+
+            return first;
+        }
+
         static void Main(string[] args)
         {
             // [1,2,6,6,3,4,5,6]
             ListNode head = new ListNode(1);
             var node1 = new ListNode(2);
-            var node2 = new ListNode(6);
-            var node3 = new ListNode(6);
-            var node4 = new ListNode(3);
-            var node5 = new ListNode(4);
-            var node6 = new ListNode(5);
-            var node7 = new ListNode(6);
+            var node2 = new ListNode(3);
+            var node3 = new ListNode(4);
+            var node4 = new ListNode(5);
+            //var node5 = new ListNode(6);
+            //var node6 = new ListNode(5);
+            //var node7 = new ListNode(6);
 
             head.next = node1;
             node1.next = node2;
             node2.next = node3;
             node3.next = node4;
-            node4.next = node5;
-            node5.next = node6;
-            node6.next = node7;
+            //node4.next = node5;
+            //node5.next = node6;
+            //node6.next = node7;
 
-            var h = RemoveElements(head, 6);
+            var h = OddEvenList(head);
         }
     }
 }
